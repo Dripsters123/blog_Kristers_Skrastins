67bb70f0ea36305e6ddf059930c3b43f08a5d28c
Your Name <you@example.com>
2024-02-26 08:56:42 +0200

    blogs

---
+++ db.sql
@@ -0,0 +1,13 @@
+/*
+CREATE DATABASE blog_Kristers_Skrastins;
+USE blog_Kristers_Skrastins;
+
+CREATE TABLE posts (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO posts (title) VALUES 
+('My First Blog Post'),('My Second Blog Post');
+*/
