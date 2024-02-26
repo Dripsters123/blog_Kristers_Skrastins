f2ea7162a71a1cb9d0d8a87ad17fb3d7c38d04f7
Your Name <you@example.com>
2024-02-26 09:48:38 +0200

    blogs.1

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>Hello!</h1>
+<?php
+echo "Hello" . "<br>";
+
+$connection_string = "mysql:host=localhost;dbname=blog_Kristers_Skrastins;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($connection_string);
+
+
+// Sagatavot SQL izpildei
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+var_dump($posts);
+echo "</pre>";
