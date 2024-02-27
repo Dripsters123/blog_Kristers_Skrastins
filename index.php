2829213dcafaffef3336636f0ad665e730301181
Your Name <you@example.com>
2024-02-27 13:26:47 +0200

    blogs.2

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+require "functions.php";
+
 echo "Hello" . "<br>";
 
 $connection_string = "mysql:host=localhost;dbname=blog_Kristers_Skrastins;user=root;password=;charset=utf8mb4";
@@ -7,8 +10,13 @@ $pdo = new PDO($connection_string);
 
 // Sagatavot SQL izpildei
 $query = $pdo->prepare("SELECT * FROM posts");
+// Izpildīt SQL
 $query->execute();
+// Beidzot dabūt rezultātus, visus pie tam!
 $posts = $query->fetchAll(PDO::FETCH_ASSOC);
-echo "<pre>";
-var_dump($posts);
-echo "</pre>";
+
+echo "<ul>";
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
+echo "</ul>";
