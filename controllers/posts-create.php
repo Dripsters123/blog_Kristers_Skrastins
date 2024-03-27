632c573f3f3abed4748635c2715755acb5f5c141
Your Name <you@example.com>
2024-03-27 14:27:35 +0200

    Datu validācija

--- controllers/posts-create.php
+++ controllers/posts-create.php
@@ -5,20 +5,36 @@ $config = require("config.php");
 $db = new Database($config);
 
 
-
+//if ($_SERVER["REQUEST_METHOD"] == "POST" && trim($_POST["title"]) != "" && $_POST["category-id"] <= 3 && strlen($_POST["title"]) <= 255) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $query = "INSERT INTO posts (title,category_id)
-    VALUES (:title,:category_id);";
+    $errors = [];
 
-    $params = [
-        ":title" => $_POST["title"],
-        ":category_id" => $_POST["category-id"]
-    ];
-    $db->execute($query, $params);
+    if (trim($_POST["title"]) == "") {
+        $errors["title"] = "Title cannot be empty";
+    }
 
-    header("Location: /");
-    die();
-};
+    if (strlen($_POST["title"]) > 255) {
+        $errors["title"] = "Title cannot be longer than 255 chars";
+    }
+
+    if ($_POST["category-id"] < 1 || $_POST["category-id"] > 3) {
+        $errors["title"] = "Category invalid";
+    }
+
+    if (empty($errors)) {
+        $query = "INSERT INTO posts (title,category_id)
+                  VALUES (:title,:category_id);";
+
+        $params = [
+            ":title" => $_POST["title"],
+            ":category_id" => $_POST["category-id"]
+        ];
+        $db->execute($query, $params);
+
+        header("Location: /");
+        die();
+    }
+}
 
 $title = "Create";
 require "views/posts-create.view.php";
